bcb9c772ef82e424daa46d247b816fe24f7860a2
Amir Hossein Amanipoor <76044261+Amirdtv@users.noreply.github.com>
2023-06-25 11:35:41 +0330

    pr

---
+++ prfect/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Perfetc
+    {
+        private int x;
+        public int X
+        {
+            get { return x; }
+            set
+            {
+                if (value > 0)
+                {
+                    x = value;
+                }
+            }
+        }
+
+        public Perfetc() { }
+        public Perfetc(int input)
+        {
+            x = input;
+        }
+        public void calc()
+        {
+            int c = 0;
+            for(int i = 1; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    c = c + i;
+                }
+            }
+            if (c == x)
+            {
+                Console.WriteLine(" perfect number ");
+            }
+            else
+            {
+                Console.WriteLine(" not perfect ");
+            }
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            Perfetc p = new Perfetc(input);
+            p.calc();
+            Console.ReadKey();
+        }
+    }
+}
